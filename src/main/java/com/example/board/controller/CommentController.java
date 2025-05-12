9e9dc57b06f4daa72ff2e762da3d4bb5a8bcc2f4
juha <sjh990715@gmail.com>
2025-05-12 16:36:46 +0900

    board: 댓글 기능 추가/detail.html에 ajax 적용 및 댓글 input,button 추가, CommentDTO, CommentController 추가

---
+++ src/main/java/com/example/board/controller/CommentController.java
@@ -0,0 +1,23 @@
+package com.example.board.controller;
+
+import com.example.board.dto.CommentDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+
+    @PostMapping("/save")
+    @ResponseBody
+    public String save(@ModelAttribute CommentDTO commentDTO) {
+        System.out.println("commentDTO: " + commentDTO);
+    // 출력: commentDTO: CommentDTO(id=null, commentWriter=댓글작성자1, commentContents=댓글내용1, boardId=1, commentCreatedTime=null)
+        return "요청 성공";
+    }
+}
